3bddd09eb14e844dfb9affa99955834b60d76884
Alex Reed <alexreed1192@gmail.com>
2020-07-19 19:28:51 -0700

    initial unit testing

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,24 +1,38 @@
 import unittest
 
 from unittest import TestCase
-from configparser import ConfigParser
+from finnews.client import News
+from finnews.cnbc import CNBC
 
 
-class MySession(TestCase):
+class TestNewsClient(TestCase):
 
-    """Will perform a unit test for the <PLACEHOLDER> session."""
+    """Will perform a unit test for the `NewsClient` session."""
 
     def setUp(self) -> None:
         """Set up the Client."""
-        pass
+
+        self.news_client = News()
 
     def test_creates_instance_of_session(self):
-        """Create an instance and make sure it's a <PLACEHOLDER>."""
-        pass
+        """Create an instance and make sure it's a `NewsClient`."""
+
+        self.assertIsInstance(self.news_client, News)
+
+    def test_creates_cnbc_instance(self):
+        """Create an instance and make sure it's a `CNBCClient`."""
+
+        # Grab the client.
+        self.cnbc_client = self.news_client.cnbc
+
+        # Make sure it's a CNBC client.
+        self.assertIsInstance(self.cnbc_client, CNBC)
+        self.assertIsNotNone(self.news_client._cnbc_client)
 
     def tearDown(self) -> None:
-        """Teardown the <PLACEHOLDER>."""
-        pass
+        """Teardown the `NewsClient`."""
+
+        del self.news_client
 
 
 if __name__ == '__main__':
